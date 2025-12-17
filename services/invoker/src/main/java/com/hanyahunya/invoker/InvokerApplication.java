0ebe24ae9c1f6d037db99589d474a28c792c2809
hanyahunya <java020103@gmail.com>
2025-12-17 12:50:13 +0900

    chore: setup initial project structure for gateway, registry, and invoker

---
+++ services/invoker/src/main/java/com/hanyahunya/invoker/InvokerApplication.java
@@ -0,0 +1,13 @@
+package com.hanyahunya.invoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvokerApplication.class, args);
+	}
+
+}
---
+++ services/invoker/src/test/java/com/hanyahunya/invoker/InvokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanyahunya.invoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
