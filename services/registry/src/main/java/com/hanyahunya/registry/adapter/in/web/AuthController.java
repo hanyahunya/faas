e036f82057ac72d6b67cc606400f4841e49e6019
hanyahunya <java020103@gmail.com>
2025-12-18 03:47:29 +0900

    refactor: AuthController 기본 경로(/auth) 추가

--- services/registry/src/main/java/com/hanyahunya/registry/adapter/in/web/AuthController.java
+++ services/registry/src/main/java/com/hanyahunya/registry/adapter/in/web/AuthController.java
@@ -13,10 +13,12 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
     private final RegisterUserUseCase registerUserUseCase;
     private final LoginUseCase loginUseCase;
