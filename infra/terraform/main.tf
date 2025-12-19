953b2309bb59590bb05827ea41a75a2d6082bef5
hanyahunya <java020103@gmail.com>
2025-12-19 23:28:27 +0900

    chore: mysql-client 설치 및 gitignore 설정 추가

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -395,7 +395,9 @@ locals {
   user_data_script = <<-EOF
     #!/bin/bash
     apt-get update
-    apt-get install -y ca-certificates curl gnupg git
+    
+    apt-get install -y ca-certificates curl gnupg git mysql-client
+
     install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     chmod a+r /etc/apt/keyrings/docker.gpg
