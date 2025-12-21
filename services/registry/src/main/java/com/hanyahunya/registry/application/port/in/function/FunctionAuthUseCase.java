93a20703ee920847f25e9f0b992f15d82d192a85
hanyahunya <java020103@gmail.com>
2025-12-21 23:46:21 +0900

    feat: 함수 검증시 s3Key 데이터도 포함하여 응답

--- services/registry/src/main/java/com/hanyahunya/registry/application/port/in/function/FunctionAuthUseCase.java
+++ services/registry/src/main/java/com/hanyahunya/registry/application/port/in/function/FunctionAuthUseCase.java
@@ -3,10 +3,15 @@ package com.hanyahunya.registry.application.port.in.function;
 import java.util.UUID;
 
 public interface FunctionAuthUseCase {
-    boolean authFunction(Command command);
+    Result authFunction(Command command);
 
     record Command(
             UUID functionId,
             String accessKey
+    ) {}
+
+    record Result(
+            boolean isValid,
+            String s3Key
     ) {}
 }
